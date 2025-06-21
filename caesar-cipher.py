ba0af16b337f8cd006e84e7332cec7ec18795c00
Mohammed23200 <mgsa112233@gmail.com>
2025-06-21 09:26:00 +0300

    the code

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,24 +1,29 @@
 import art
-print(
-art.logo
-)
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-restert = True
-while restert:
+print(art.logo)
+
+alphabet = [chr(i) for i in range(97, 123)]  # نفس ['a' to 'z']
+
+restart = True
+while restart:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
-    def caesar(original_text, shift_amount,dir):
+    def caesar(original_text, shift_amount, dir):
         cipher_text = ""
+        if dir == "decode":
+            shift_amount *= -1
         for letter in original_text:
             if letter in alphabet:
-                if dir == "decode":
-                    shift_amount*=-1
-            shift_position = (alphabet.index(letter) + shift_amount) % len(alphabet)
-            cipher_text += alphabet[shift_position]
-        print(f"Here is the {dir}: {cipher_text}")
-    caesar(text,shift,direction)
-    state=input('you want to continue or exit')
+                shift_position = (alphabet.index(letter) + shift_amount) % len(alphabet)
+                cipher_text += alphabet[shift_position]
+            else:
+                cipher_text += letter
+        print(f"Here is the {dir}d result: {cipher_text}")
+
+    caesar(text, shift, direction)
+
+    state = input("Type 'yes' to go again. Otherwise type 'exit':\n").lower()
     if state == 'exit':
-        restert = False
+        restart = False
+        print("Goodbye!")
